a3fe0bab3e27ae24f76193ddc0e8820bacaf843e
TamimSyed <tamimsyedblogging@gmail.com>
2017-08-09 21:14:05 -0700

    New build tool changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,81 @@
-var fileinclude = require('gulp-file-include'),
-  gulp = require('gulp');
-  var del = require('del');
-  var rename = require("gulp-rename");
+var fileinclude = require('gulp-file-include');
+var gulp = require('gulp');
+var del = require('del');
+var rename = require("gulp-rename");
+var each = require('gulp-each');
+var foreach = require('gulp-foreach');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
 
-gulp.task('processing', function() {
-  gulp.src(['markup.html'])
+const arrayHtmlFiles = [];
+const arrayCssFiles = [];
+
+gulp.task('processing',['delete'],function () {
+
+  return gulp.src('./**/markup.html')
+    .pipe(foreach(function(stream, file){
+      var pathFile = file.path;
+      var finalPath = pathFile.substring(0,pathFile.lastIndexOf("/")+1);
+      arrayHtmlFiles.push(finalPath);
+    //  console.log("finalPath >> "+finalPath+" >> length "+arrayHtmlFiles.length);
+      gulp.src(['/Applications/MAMP/htdocs/thetruthengineland/**/markup.html'])
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '/Applications/MAMP/htdocs/thetruthengineland/production'
+        }))
+        .pipe(rename("index.html"));
+      return stream
+    }))
+});
+
+gulp.task('css',function () {
+
+  return gulp.src('./**/*.css')
+    .pipe(foreach(function(stream, file){
+      var pathFile = file.path;
+      var finalPath = pathFile.substring(0,pathFile.lastIndexOf("/")+1);
+      if (!(pathFile.indexOf("node_modules")>=0))
+        arrayCssFiles.push(finalPath);
+      return stream
+    }))
+});
+
+gulp.task('sitemaphtml',['processing'],function () {
+  gulp.src(['./sitemapmarkup.html'])
     .pipe(fileinclude({
       prefix: '@@',
-      basepath: '@file'
+      basepath: '/Applications/MAMP/htdocs/thetruthengineland/production/'
     }))
-    .pipe(rename("index.html"))
+    .pipe(rename("sitemap.html"))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('move', function() {
-// rename via string
-gulp.src("./build/*.html")
-  .pipe(rename("index.html")).pipe(gulp.dest("./"));
+gulp.task('default',['css-iterate','sitemaphtml'], function () {
+    for(var member of arrayHtmlFiles){
+      gulp.src([member+'markup.html'])
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '/Applications/MAMP/htdocs/thetruthengineland/production/'
+        }))
+        .pipe(minifyHTML({ conditionals: true, spare:true}))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest(member));
+    }
+});
+
+gulp.task('css-iterate',['css'], function () {
+    for(var member of arrayCssFiles){
+      gulp.src(member+"/*.css")
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest(member));
+    }
 });
 
 gulp.task('delete', function () {
   return del([
-    './index.html'
+    './**/index.html',
+    './sitemap.html',
+    '!./common/header/index.html',
+    '!./common/footer/index.html'
   ]);
 });
-
-// gulp.task('copy', function () {
-//     gulp.src('markup.html')
-//         .pipe(gulp.dest('./copyFile'));
-// });
